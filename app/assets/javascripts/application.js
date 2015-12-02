e6ea71296738481ea169e8ae60790196cbfc69ac
Pat <pat@ditto.us.com>
2015-12-02 01:16:22 -0500

    site basic redesign; header image full height, tagline, block section, footer; footer links keyframe brand color by id; less optimal for older IE versions, but assuming this audience isn't visiting my site

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,19 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    // https://css-tricks.com/snippets/jquery/smooth-scrolling/
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
-    }
-  });
-});
--- app/assets/javascripts/pat.js
+++ app/assets/javascripts/pat.js
@@ -1,1 +1,4 @@
 console.log('if you can read this, you are probably a web developer :D');
+
+$(document).ready(function(){
+});
