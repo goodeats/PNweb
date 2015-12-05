236d85e828ceeeafff7cf4f77792d1eda53f4c9a
Pat <pat@ditto.us.com>
2015-12-05 13:02:38 -0500

    put window.scroll in else of chrome iOS browser, trying smoot scrolling on skrollr-body; will need to find way to test chrome browser iOS in dev so I don't have to push to production to test

--- app/assets/javascripts/pat.js
+++ app/assets/javascripts/pat.js
@@ -14,36 +14,36 @@ $(document).ready(function(){
       smoothScrolling: false,
       mobileDeceleration: 0.004
     });
+  } else {
+    $(window).on('scroll', function(){
+      console.log('ahhh! I\'m falling!')
+      var from_top = $(window).scrollTop();
+      var above_one = 1 + from_top / 2000;
+      var below_one = 1 - from_top / 2000;
+      var rotate_left = -from_top / 50;
+      var from_middle = 50 - rotate_left;
+      $('#scroll').text(above_one);
+
+      $('.nav-content').css({
+        '-webkit-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
+        '-moz-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
+        '-o-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
+        'transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
+        'background-position': from_middle + '% 50%',
+        'margin-left': -from_top / 20 + 'px',
+        'margin-bottom': -from_top / 10 + 'px',
+      });
+
+      $('.nav-title-name').css({
+        '-webkit-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
+        '-moz-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
+        '-o-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
+        'transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
+        'right': 10 + from_top / 100 + '%',
+        'letter-spacing': -5 + -rotate_left + 'px'
+      });
+
+    });
   }
-
-  $(window).on('scroll', function(){
-    console.log('ahhh! I\'m falling!')
-    var from_top = $(window).scrollTop();
-    var above_one = 1 + from_top / 2000;
-    var below_one = 1 - from_top / 2000;
-    var rotate_left = -from_top / 50;
-    var from_middle = 50 - rotate_left;
-    $('#scroll').text(above_one);
-
-    $('.nav-content').css({
-      '-webkit-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
-      '-moz-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
-      '-o-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
-      'transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
-      'background-position': from_middle + '% 50%',
-      'margin-left': -from_top / 20 + 'px',
-      'margin-bottom': -from_top / 10 + 'px',
-    });
-    
-    $('.nav-title-name').css({
-      '-webkit-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
-      '-moz-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
-      '-o-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
-      'transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
-      'right': 10 + from_top / 100 + '%',
-      'letter-spacing': -5 + -rotate_left + 'px'
-    });
-
-  });
 });
 
