bc21e560a1786148f92aafc2fa28741ed58aa3b0
Pat <pat@ditto.us.com>
2015-12-04 23:12:38 -0500

    added variables for time and font-size; header background image is a compressed file; moved contact hover keyframes to mixins - can go deeper; scroll down zooms on background image - very jaggedy

--- app/assets/javascripts/pat.js
+++ app/assets/javascripts/pat.js
@@ -1,4 +1,28 @@
 console.log('if you can read this, you are probably a web developer :D');
 
 $(document).ready(function(){
+  var div = '<h3 id="scroll">1</h3>';
+  $('.nav-content').append(div);
+  $('#scroll').css({
+    'position': 'absolute',
+    'top': '50%',
+    'left': '25%',
+    'color': '#fff'
+  });
+
+  $(window).on('scroll', function(){
+    var scroll_spot = $(window).scrollTop();
+    $('#scroll').text(scroll_spot);
+    var zoom = 1 + scroll_spot / 1000;
+
+    $('.nav-content').css({
+      '-moz-transform': 'scale(' + zoom + ',' + zoom + ')',
+      '-webkit-transform': 'scale(' + zoom + ',' + zoom + ')',
+      'transform': 'scale(' + zoom + ',' + zoom + ')'
+    });
+    
+
+
+  });
 });
+
