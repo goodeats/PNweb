99c368ec0a35703f6b647d06085abbba5e16375a
Pat <pat@ditto.us.com>
2015-12-05 13:30:28 -0500

    commenting out js testing and saving them for later

--- app/assets/javascripts/pat.js
+++ app/assets/javascripts/pat.js
@@ -2,26 +2,27 @@ console.log('if you can read this, you are probably a web developer :D');
 
 $(document).ready(function(){
 
-  var sec = 0;
-  function pad(val) {
-    return val > 9 ? val : "0" + val;
-  }
-  var timer = setInterval(function () {
-    document.getElementById("seconds").innerHTML = pad(++sec % 60);
-    document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
-  }, 1000);
-  setTimeout(function () {
-    clearInterval(timer);
-  }, 11000);
+  // var sec = 0;
+  // function pad(val) {
+  //   return val > 9 ? val : "0" + val;
+  // }
+  // var timer = setInterval(function () {
+  //   document.getElementById("seconds").innerHTML = pad(++sec % 60);
+  //   document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+  // }, 1000);
+  // setTimeout(function () {
+  //   clearInterval(timer);
+  // }, 11000);
 
-  var div = "<h4 id='scroll'></h4>";
-  $('.nav-content').append(div);
-  $('#scroll').css({
-    'position': 'absolute',
-    'top': '50%',
-    'right': '20%',
-    'color': '#fff'
-  });
+  // for testing only the scrolltop
+  // var div = "<h4 id='scroll'></h4>";
+  // $('.nav-content').append(div);
+  // $('#scroll').css({
+  //   'position': 'absolute',
+  //   'top': '50%',
+  //   'right': '20%',
+  //   'color': '#fff'
+  // });
   if(navigator.userAgent.match('CriOS')){
     alert('urine chrome ios!');
     $('.nav-title-name').text('Pat \"too cool for school\" Needham');
