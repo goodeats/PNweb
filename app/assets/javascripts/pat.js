98aaf889bd02d494bc757c956ec22f00ffbbaba2
pat <patrick.h.needham@gmail.com>
2015-12-06 14:08:21 -0500

    added jquery-ui gem and shake.js for setting up some tools for mobile experience

--- app/assets/javascripts/pat.js
+++ app/assets/javascripts/pat.js
@@ -1,7 +1,51 @@
 console.log('if you can read this, you are probably a web developer :D');
+// var Shake = require('shake.js');
+
+init();
+var count = 0;
+
+function init() {
+  if (window.DeviceOrientationEvent) {
+    // document.getElementById("doEvent").innerHTML = "DeviceOrientation";
+    // Listen for the deviceorientation event and handle the raw data
+    window.addEventListener('deviceorientation', function(eventData) {
+      // gamma is the left-to-right tilt in degrees, where right is positive
+      var tiltLR = eventData.gamma;
+
+      // beta is the front-to-back tilt in degrees, where front is positive
+      var tiltFB = eventData.beta;
+
+      // alpha is the compass direction the device is facing in degrees
+      var dir = eventData.alpha;
+
+      // call our orientation event handler
+      deviceOrientationHandler(tiltLR, tiltFB, dir);
+      }, false);
+  } else {
+    // document.getElementById("doEvent").innerHTML = "Not supported on your device or browser.  Sorry."
+  }
+}
+
+function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+  // document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+  // document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+  // document.getElementById("doDirection").innerHTML = Math.round(dir);
+
+  // Apply the transform to the image
+  var logo = document.getElementById("pat-logo");
+  logo.style.webkitTransform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+  logo.style.MozTransform = "rotate("+ tiltLR +"deg)";
+  logo.style.transform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+}
+
+// Some other fun rotations to try...
+//var rotation = "rotate3d(0,1,0, "+ (tiltLR*-1)+"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+//var rotation = "rotate("+ tiltLR +"deg) rotate3d(0,1,0, "+ (tiltLR*-1)+"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
 
 $(document).ready(function(){
-
+  if (window.DeviceOrientationEvent) {
+   console.log("DeviceOrientation is supported");
+  }
   // var sec = 0;
   // function pad(val) {
   //   return val > 9 ? val : "0" + val;
