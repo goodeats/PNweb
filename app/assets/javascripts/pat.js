617e5dfae5928969fbfe6be453296621f18356aa
pat <patrick.h.needham@gmail.com>
2016-01-29 22:06:27 -0500

    added script from google drive; changed paragraph copy; removed header image tilt, keeping title tilt and spacing

--- app/assets/javascripts/pat.js
+++ app/assets/javascripts/pat.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
     });
   } else {
     $(window).on('scroll', function(){
-      console.log('ahhh! I\'m falling!')
+      // console.log('ahhh! I\'m falling!');
       var from_top = $(window).scrollTop();
       var above_one = 1 + from_top / 2000;
       var below_one = 1 - from_top / 2000;
@@ -72,15 +72,15 @@ $(document).ready(function(){
       var from_middle = 50 - rotate_left;
       $('#scroll').text(above_one);
 
-      $('.nav-content').css({
-        '-webkit-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
-        '-moz-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
-        '-o-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
-        'transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
-        'background-position': from_middle + '% 50%',
-        'margin-left': -from_top / 20 + 'px',
-        'margin-bottom': -from_top / 10 + 'px',
-      });
+      // $('.nav-content').css({
+      //   '-webkit-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
+      //   '-moz-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
+      //   '-o-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
+      //   'transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + rotate_left + 'deg)',
+      //   'background-position': from_middle + '% 50%',
+      //   'margin-left': -from_top / 20 + 'px',
+      //   'margin-bottom': -from_top / 10 + 'px',
+      // });
 
       $('.nav-title-name').css({
         '-webkit-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
