fd2f69cd82de31437851c97fdb5fc627e9a34cfe
Pat <patrick.h.needham@gmail.com>
2018-07-26 00:11:32 -0400

    not pulling away title -- was implemented to simulate 'hero' image falling off the mountain I guess

--- app/assets/javascripts/pat.js
+++ app/assets/javascripts/pat.js
@@ -5,7 +5,6 @@
   function init(){
     externalLinksListener();
     deviceOrientationListener();
-    headerScrollListener();
   }
 
   function externalLinksListener(){
@@ -74,41 +73,6 @@
     logo.style.webkitTransform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
        logo.style.MozTransform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
           logo.style.transform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
-  }
-
-  function headerScrollListener(){
-    // header pullaway doesn't work while scrolling in chrome
-    if (!navigator.userAgent.match('CriOS')){
-      $(window).on('scroll', function(){
-        var from_top = $(window).scrollTop();
-        var above_one = 1 + from_top / 2000;
-        var below_one = 1 - from_top / 2000;
-        var rotate_left = -from_top / 50;
-        var from_middle = 50 - rotate_left;
-
-        // rotate header and zoom
-        // commented out, since it's confusing and jaggedy for mobile
-        // $('.nav-content').css({
-        //   'background-position': from_middle + '% 50%',
-        //   'margin-left': -from_top / 20 + 'px',
-        //   'margin-bottom': -from_top / 10 + 'px',
-        //   '-webkit-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + (rotate_left / 100) + 'deg)',
-           //   '-moz-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + (rotate_left / 100) + 'deg)',
-             //   '-o-transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + (rotate_left / 100) + 'deg)',
-                //   'transform': 'scale(' + above_one + ',' + above_one + ') rotate(' + (rotate_left / 100) + 'deg)',
-        // });
-
-        // rotate title, space letters
-        $('.nav-title-name').css({
-          'right': 10 + from_top / 100 + '%',
-          'letter-spacing': -5 + -rotate_left + 'px',
-          '-webkit-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
-             '-moz-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
-               '-o-transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)',
-                  'transform': 'scale(' + below_one + ',' + below_one + ') rotate(' + rotate_left + 'deg)'
-        });
-      });
-    }
   }
 
   $(document).on('ready', function(){
