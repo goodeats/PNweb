cc6c73ceb8c0afe27b105c4c3a57421cb65ebbf4
Pat <patrick.h.needham@gmail.com>
2018-08-06 13:06:23 -0400

    can update projects

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -19,4 +19,19 @@ class Admin::ProjectsController < ApplicationController
     add_breadcrumb 'edit', edit_admin_project_path(@project)
   end
 
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to admin_project_path(@project)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :published, :external, :url, :description)
+  end
+
 end
