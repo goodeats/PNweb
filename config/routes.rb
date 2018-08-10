2f936c3567cf4c9cf475c05f11ac4c1fe42e8c55
Pat <patrick.h.needham@gmail.com>
2018-08-10 13:53:18 -0400

    demos available to admin, new guest view

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :pat, only: [:index]
-    resources :projects, :about
+    resources :projects, :demos, :about
     resources :articles, path: 'blog'
   end
 
