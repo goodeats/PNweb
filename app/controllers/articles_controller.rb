4dfcae5ef118ef14dcb6c4efc20f57a910bf8bb3
Pat <patrick.h.needham@gmail.com>
2018-08-11 14:31:38 -0400

    blog set with meta

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,25 @@
 class ArticlesController < ApplicationController
+  include SetMetaTags
   skip_before_action :authenticate_user!
+  before_action :set_meta_articles
 
   add_breadcrumb '🏠', :root_path
   add_breadcrumb 'blog', :articles_path
 
   def index
     @articles = Article.published.desc
+  end
+
+  private
+
+  def set_meta_articles
     @page_title = 'blog'
     @page_description = "Blog by Pat Needham"
-    @page_keywords = @page_keywords + ['blog', 'articles']
+    @page_keywords = @page_keywords + keywords_articles
   end
+
+  def keywords_articles
+    ['blog', 'articles']
+  end
+
 end
