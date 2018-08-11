d9232fdf05e797b43e2aebec958a6ed7887016fe
Pat <patrick.h.needham@gmail.com>
2018-08-11 14:38:06 -0400

    set demos meta

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -1,23 +1,19 @@
 class DemosController < ApplicationController
+  include SetMetaTags
   skip_before_action :authenticate_user!
-  before_action :set_demo, only: [:show]
+  before_action :set_meta_demos, only: [:index]
+  before_action :set_demo, :set_meta_demo, only: [:show]
 
   add_breadcrumb '🏠', :root_path
   add_breadcrumb 'demos', :demos_path
 
   def index
     @demos = Demo.published.desc
-    @page_title = 'my demos'
-    @page_description = 'Demos by Pat Needham'
-    @page_keywords = @page_keywords + ['demos', 'react', 'magiceye', 'magic', 'eye']
   end
 
   def show
     if @demo.published
       add_breadcrumb @demo.title, demo_path(@demo)
-      @page_title = @demo.title
-      @page_description = "#{@demo.title} | Demos by Pat Needham"
-      @page_keywords = @page_keywords + ['demos', 'demo', 'react', 'magiceye', 'magic', 'eye']
     else
       redirect_to demos_path
     end
@@ -27,5 +23,21 @@ class DemosController < ApplicationController
 
   def set_demo
     @demo = Demo.find(params[:id])
+  end
+
+  def set_meta_demos
+    @page_title = 'my demos'
+    @page_description = 'Demos by Pat Needham'
+    @page_keywords = @page_keywords + keywords_demos
+  end
+
+  def set_meta_demo
+    @page_title = @demo.title
+    @page_description = "#{@demo.title} | Demos by Pat Needham"
+    @page_keywords = @page_keywords + keywords_demos
+  end
+
+  def keywords_demos
+    ['demos', 'react', 'magiceye', 'magic', 'eye']
   end
 end
