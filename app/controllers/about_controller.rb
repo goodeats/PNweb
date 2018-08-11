23074e9b87a339357b64d9c37bf58f2a7012c4b2
Pat <patrick.h.needham@gmail.com>
2018-08-11 13:48:26 -0400

    resetting format for meta to include open graph and twitter

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,13 +1,47 @@
 class AboutController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_meta_about
 
   add_breadcrumb '🏠', :root_path
   add_breadcrumb 'about', :about_index_path
 
   def index
     @abouts = About.published.asc
+  end
+
+  private
+
+  def set_meta_about
     @page_title = 'about me'
     @page_description = 'About Pat Needham'
-    @page_keywords = @page_keywords + ['about', 'me', 'bio', 'professional', 'personal', 'opencity', 'ditto', 'general', 'assembly', 'wdi']
+    @page_keywords = @page_keywords + keywords_about
+    set_meta_tags og: og_tags,
+                  twitter: twitter_tags
   end
+
+  def keywords_about
+    ['about', 'me', 'bio', 'professional', 'personal', 'opencity', 'ditto', 'general', 'assembly', 'wdi']
+  end
+
+  def og_tags
+    {
+      site_name:    'Pat Needham',
+      title:        'about me',
+      description:  'About Pat Needham',
+      image:        @image_src,
+      url:          request.env['REQUEST_URI']
+    }
+  end
+
+  def twitter_tags
+    {
+      card:         'summary',
+      site:         '@patneedham_',
+      title:        'about me',
+      description:  'About Pat Needham',
+      image:        @image_src,
+      url:          request.env['REQUEST_URI']
+    }
+  end
+
 end
