a22a65b3b019175d6fbfeb17fcd75d100daa1493
Pat <patrick.h.needham@gmail.com>
2020-02-03 19:32:34 -0800

    skip auth

--- app/controllers/api/v1/yo_controller.rb
+++ app/controllers/api/v1/yo_controller.rb
@@ -1,15 +1,8 @@
 class Api::V1::YoController < ApplicationController
-  # before_action :ensure_json_request
+  skip_before_action :authenticate_user!
 
   def yo
     render json: { yo: 'yo' }
   end
 
-  # private
-
-  # def ensure_json_request
-  #   return if request.format == :json
-
-  #   render nothing: true, status: 406
-  # end
 end
